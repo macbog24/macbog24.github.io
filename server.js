e9751d9f8f1fdf5af2692745abe455d31f32e5a3
Mackenzie Bogiages <mbogiages3@gatech.edu>
2026-09-08 12:16:15 -0700

    Fix resume CORS

--- server.js
+++ server.js
@@ -8,6 +8,18 @@ const PORT = process.env.PORT || 3000;
 const RESUME_PASSWORD = process.env.RESUME_PASSWORD || 'Sonic18!';
 const PDF_PATH = path.join(__dirname, 'assets', 'Bogiages_Mackenzie_resume.pdf');
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+
+  next();
+});
+
 app.use(express.json());
 app.use(express.static(__dirname));
 
